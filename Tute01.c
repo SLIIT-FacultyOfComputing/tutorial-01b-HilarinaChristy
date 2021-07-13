ec312b6e0539bbbc26ab986a26f875fdd2b1fdeb
IT21127946 <86715390+HilarinaChristy@users.noreply.github.com>
2021-07-13 03:40:51 +0000

    Variables

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,16 @@
 #include <stdio.h>
 
 int main() {
-  
+  int n1,n2,tot;
+  float avg;
+  printf("Enter num1:");
+  scanf("%d",&n1);
+  printf("Enter num2:");
+  scanf("%d",&n2);
+  tot = n1 + n2;
+  printf("Sum is %d",tot);
+  avg = tot / 2.0;
+  printf("The average is %d",avg);
   return 0;
 }
 
